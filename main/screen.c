a38072eb71b1916a2efdcb35e0aeac1f5fc19b54
Daniel BORNAZ <daniel.bornaz@gmail.com>
2026-04-11 07:41:44 -0700

    fully working

--- main/screen.c
+++ main/screen.c
@@ -15,22 +15,33 @@ bool mat_init = 0;
 uint16_t scr_sq_size, mat_dim;
 
 /**
- * @brief Convert 1D array to 2D array
- *
- * Return the i and j indexes in a bidimensional array from
- * a linear array index idx
- *
- * @param[idx] linear array index
- * @param[i]
- * @param[j] pointers to return values
- *
- * @return
- *		no return
+ * @brief Rotates a given line left/right
+ * @param(uint16_t) dir: 0 = rotate left, 1 = rotate right
  */
-void mat_idx_to_2d(uint16_t idx, uint16_t *i, uint16_t *j){
+void mat_rotate_line(uint16_t line, uint16_t dir, uint16_t count) {
+	circle_t tmp;
 
-	*i = idx % mat_dim;
-	*j = idx / mat_dim;
+	if ( dir == 0 ) {
+		tmp = mat_circle[line * mat_dim];
+		for( int cnt=0; cnt < count; cnt++){
+			for( int i=0; i < (mat_dim - 1); i++)
+				mat_circle[line * mat_dim + i] = mat_circle[line * mat_dim + i + 1];
+			mat_circle[(line + 1) * mat_dim - 1] = tmp;
+		}
+	} else {
+		tmp = mat_circle[(line + 1) * mat_dim - 1];
+		for( int cnt=0; cnt < count; cnt++){
+			for( int i=(mat_dim - 1); i > 0; i--)
+				mat_circle[line * mat_dim + i] = mat_circle[line * mat_dim + i - 1];
+			mat_circle[line * mat_dim] = tmp;
+		}
+	}
+}
+
+/**
+ * @brief Rotates a given column up/down
+ */
+void mat_rotate_col(uint16_t col, uint16_t dir, uint16_t count) {
 }
 
 /**
@@ -48,8 +59,11 @@ void mat_populate(void) {
 		for( j=0; j< mat_dim; j++) {
 			mat_circle[i*mat_dim + j].x = i * rmax + rmax/2 - 1;
 			mat_circle[i*mat_dim + j].y = j * rmax + rmax/2 - 1;
+			// ESP_LOGI("TAG","%6u: %6u %6u", i*mat_dim + j, mat_circle[i*mat_dim + j].x, mat_circle[i*mat_dim + j].y);
 		}
 	}
+	for( i=0; i< mat_dim; i++)
+		mat_rotate_line(i, 1, 2);
 }
 
 /**
@@ -63,7 +77,7 @@ void mat_setup(u8g2_t *u8g2_in, uint16_t resolution, uint16_t divisions) {
 
 	mat_init = true;
 
-	mat_circle = malloc(sizeof(circle_t) * divisions * divisions);
+	mat_circle = malloc(sizeof(circle_t) * mat_dim * mat_dim);
 
 	if(!mat_circle) {
 		ESP_LOGE("TAG", "Error allocating memory");
@@ -81,7 +95,7 @@ void mat_setup(u8g2_t *u8g2_in, uint16_t resolution, uint16_t divisions) {
 void mat_plot(VL53L8CX_ResultsData 	results) {
 	uint16_t i, j;
 	uint16_t r;
-	float af = 7.0/400.0;
+	float af = 8.0/300.0;
 	float rf;
 
 	if(!mat_init)
@@ -89,14 +103,15 @@ void mat_plot(VL53L8CX_ResultsData 	results) {
 
 	u8g2_ClearBuffer(u8g2);
 
-	for( i=0; i< mat_dim; i++) {
-		for( j=0; j< mat_dim; j++) {
+	for( i=0; i < mat_dim; i++) {
+		for( j=0; j < mat_dim; j++) {
 			r = results.distance_mm[VL53L8CX_NB_TARGET_PER_ZONE * (i * mat_dim + j)];
 			rf = (float)r * af;
 			r = (uint16_t)rf;
-			if (r > 7) r = 7;
-			r = 7 - r;
+			if (r > 8) r = 8;
+			r = 8 - r;
 			u8g2_DrawDisc(u8g2, mat_circle[i*mat_dim + j].x, mat_circle[i*mat_dim +j].y, r, U8G2_DRAW_ALL);
+			//u8g2_DrawCircle(u8g2, mat_circle[i*mat_dim + j].x, mat_circle[i*mat_dim +j].y, r, U8G2_DRAW_ALL);
 		}
 	}
 
